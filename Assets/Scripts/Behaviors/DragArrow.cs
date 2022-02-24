b1692539dc1c425fcb504761a4f3857acd94bb91
Marshall Krueger <mgkrueger4@gmail.com>
2022-02-24 09:54:58 -0700

    Adding drag-arrow functionality

--- Assets/Scripts/Behaviors/DragArrow.cs
+++ Assets/Scripts/Behaviors/DragArrow.cs
@@ -1,17 +1,52 @@
 
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class DragArrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 origin;
+    private Vector3 target;
+    public float baseHeadScale = 0.4f;
+    private float relativeHeadScale;
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
     {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    public void InitializeArrow(Vector3 newOrigin)
+    {
+        origin = newOrigin;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateArrow(Vector3 newTarget)
     {
+        target = newTarget;
+    }
+
+    private void CalculateArrow()
+    {
+        _lineRenderer.widthCurve = new AnimationCurve(
+            new Keyframe(0, 0.4f),
+            new Keyframe(0.999f - baseHeadScale, 0.4f),
+            new Keyframe(1 - baseHeadScale, 1f),
+            new Keyframe(1f, 0f)
+            );
         
+        _lineRenderer.SetPositions(new Vector3[]
+        {
+            origin,
+            Vector3.Lerp(origin, target, 0.999f - baseHeadScale),
+            Vector3.Lerp(origin, target, 1f - baseHeadScale),
+            target
+        });
+    }
+
+    public void DestroyArrow()
+    {
+        Destroy(gameObject);
     }
 }
